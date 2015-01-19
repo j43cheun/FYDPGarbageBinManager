d6fb37395c06bdcb33e1967e56f15dcd390f6169
Evan <evgeny.nam.2012@gmail.com>
2015-01-18 21:52:19 -0500

    Added backend code to query DB and return all spots in JSON array

--- GarbageBinServer/src/com/garbagebinserver/servlets/GarbageSpotsServlet.java
+++ GarbageBinServer/src/com/garbagebinserver/servlets/GarbageSpotsServlet.java
@@ -1,6 +1,7 @@
 package com.garbagebinserver.servlets;
 
 import java.io.IOException;
+import java.sql.*;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
@@ -55,9 +57,47 @@ public class GarbageSpotsServlet extends HttpServlet {
 		    
 		    switch( action ) {
 		      case "displaySpots":
+		    	
+		    	Connection conn = null;
+		    	Statement statement = null;
+		    	ResultSet results; 
+		    	//Create a new SQL test statement
+				String constructing = "SELECT * FROM `garbagespot` WHERE 1";
+				JSONObject finalObject = new JSONObject();
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					
+					//Set up the connection to the database on port 3306 (default)
+					//To database robot1
+					//With username root and password fydp
+					conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/trash", "root", "");
+
+					//Perform a query
+					statement = conn.createStatement();
+					results = statement.executeQuery(constructing);
+					JSONArray allSpots = new JSONArray();
+					while (results.next()) {
+					    JSONObject garbageSpot = new JSONObject();
+					    
+					    garbageSpot.put("id", results.getInt("id"));
+					    garbageSpot.put("name", results.getString("name"));
+					    garbageSpot.put("latitude", results.getDouble("latitude"));
+					    garbageSpot.put("longitude", results.getDouble("longitude"));
+					    
+					    allSpots.add(garbageSpot);
+					    
+					}
+					finalObject.put("allSpots", allSpots);
+					
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		    	   
 		        jsonDataRequestString = ( String )request.getParameter( "json" );
 		        jsonDataRequestObject = ( JSONObject )JSONValue.parse( jsonDataRequestString );
-		        jsonDataResponseObject.put( "response", "Hello from servlet" );
+		        
+		        jsonDataResponseObject.put( "response", finalObject );
 		        
 		        break;
 		      default:
